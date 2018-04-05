a1e23a81ee73c5ff5b28c1bc7a64b8e295db3974
白石洋介 <y-shiraishi@shiraishiyousukes-MacBook-Pro.local>
2018-04-05 14:07:26 +0900

    README.mdにアプリ情報記載

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,10 @@ module DiveIntoShare
     config.i18n.default_locale = :ja
     config.active_record.default_timezone = :local
     config.time_zone = 'Tokyo'
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
