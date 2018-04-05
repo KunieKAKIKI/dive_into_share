7c4f62dbb639fefa7bc2e75f8512eb8849f59aee
白石洋介 <y-shiraishi@shiraishiyousukes-MacBook-Pro.local>
2018-04-05 15:30:22 +0900

    application.rbのコメント削除

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,8 @@
 require_relative 'boot'
-
 require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module DiveIntoShare
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.available_locales = [:en, :ja]
     config.i18n.default_locale = :ja
@@ -18,8 +12,5 @@ module DiveIntoShare
       g.assets     false
       g.helper     false
     end
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
