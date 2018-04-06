408cafde611240312ba1758e43a6e44b76fe33a7
kakiki <kakikunie@icloud.com>
2018-04-06 17:04:32 +0900

    ユーザモデルとマイグレーションファイル作成

---
+++ db/migrate/20180406080207_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :name, :string, null: false, default: ""
+    add_column :users, :description, :text
+    add_column :users, :image, :string
+  end
+end
