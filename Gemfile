a9ed43bb4ec12e078e890ddbaa3df8e1d8124a61
白石洋介 <y-shiraishi@shiraishiyousukes-MacBook-Pro.local>
2018-04-06 20:44:27 +0900

    gem toastrの設定

--- Gemfile
+++ Gemfile
@@ -38,3 +38,4 @@ end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'slim-rails'
+gem 'toastr-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery/dist/jquery.min.js
 //= require rails-ujs
 //= require turbolinks
+//= require toastr
 //= require_tree .
