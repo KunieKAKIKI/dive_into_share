640d46621edad0cea4571cb88cfe52f2500f8a7c
白石洋介 <y-shiraishi@shiraishiyousukes-MacBook-Pro.local>
2018-04-06 10:55:14 +0900

    ローカル時間設定削除

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ module DiveIntoShare
     config.load_defaults 5.1
     config.i18n.available_locales = [:en, :ja]
     config.i18n.default_locale = :ja
-    config.active_record.default_timezone = :local
-    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.assets false
       g.helper false
