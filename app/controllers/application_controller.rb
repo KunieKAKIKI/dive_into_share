48d97ce46deb59787605a1d6681dbbc52ac08e36
白石洋介 <y-shiraishi@shiraishiyousukes-MacBook-Pro.local>
2018-04-06 20:46:40 +0900

    toastr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  add_flash_types :success, :info, :warning, :danger
   http_basic_authenticate_with name: ENV.fetch('BASIC_AUTH_USERNAME'),
    password: ENV.fetch('BASIC_AUTH_PASSWORD') if Rails.env.staging?
 end
