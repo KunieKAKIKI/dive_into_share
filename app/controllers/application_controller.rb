b4da33c8735b3c5970dbac6378274e2742f978c4
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-04-08 17:38:48 +0900

    サイドメニュー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
 
   http_basic_authenticate_with name: ENV.fetch('BASIC_AUTH_USERNAME'),
    password: ENV.fetch('BASIC_AUTH_PASSWORD') if Rails.env.staging?
+
+   before_action :side_menu
+
+   private
+   def side_menu
+     @teams = Team.all.includes(categories: :posts)
+   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   belongs_to :team
   has_many :categories, dependent: :destroy
+  has_many :posts, dependent: :destroy
 end
