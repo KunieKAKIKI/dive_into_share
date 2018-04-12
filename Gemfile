d29ab26f53d0dc73b2a82405efe89056612d854c
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-04-12 11:03:12 +0900

    Revert "flashのメッセージ作成"
    This reverts commit aee4c58f5b426b93da5f4429f22234d01d69e0da.

--- Gemfile
+++ Gemfile
@@ -40,4 +40,3 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'slim-rails'
 gem 'devise'
-gem 'html2slim'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
   http_basic_authenticate_with name: ENV.fetch('BASIC_AUTH_USERNAME'),
    password: ENV.fetch('BASIC_AUTH_PASSWORD') if Rails.env.staging?
 
