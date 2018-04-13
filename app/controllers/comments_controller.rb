a5e3ef70f0fc2b19a56cd32b5c5be44eef41ebe6
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-04-13 16:08:48 +0900

    コメント削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,8 @@ class CommentsController < ApplicationController
       if @comment.save
         format.js { render :index }
       else
-        format.html { redirect_to post_path(@post), notice: '投稿できませんでした' }
+        format.js { render :index }
+        #format.html { redirect_to post_path(@post), notice: '投稿できませんでした' }
       end
     end
   end
