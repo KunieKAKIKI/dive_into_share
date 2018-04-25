fdc4a30843d4ff6c96b3cb8e138733df72fdfd93
kakiki <kakikunie@icloud.com>
2018-04-25 16:56:56 +0900

    memberships_controller.rb編集、招待画面編集

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,13 +3,13 @@ class MembershipsController < ApplicationController
     @user = User.find(params[:user_id])
     @team = Team.find(params[:team_id])
     membership = @user.memberships.create(team_id: @team.id)
-    redirect_to team_users_path, notice: "#{@user.name}さんを招待しました。"
+    redirect_to team_users_path(@team), notice: "#{@user.name}さんを招待しました。"
   end
 
   def destroy
     @user = User.find(params[:user_id])
     @team = Team.find(params[:team_id])
     membership = @user.memberships.find_by(team_id: @team).destroy
-    redirect_to team_users_path, notice: "#{@user.name}さんがチームから退会しました。"
+    redirect_to team_users_path(@team), notice: "#{@user.name}さんがチームから退会しました。"
   end
 end
