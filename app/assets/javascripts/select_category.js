2a70184e196000440e9001ad234ac85e79d36a44
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-04-26 21:49:31 +0900

    カテゴリ名取得js修正

--- app/assets/javascripts/select_category.js
+++ app/assets/javascripts/select_category.js
@@ -1,22 +1,24 @@
 //セレクトボックス1でチームを選択すると、セレクトボックス2にカテゴリを表示
-$(window).on('load', function(){
-
-  $('#select_team').change(function() {
+$(function() {
+  $('#select_team').on('change', function() {
     $.ajax({
       type: "GET",
-      url: $('#select_team').find('option:selected').data().path,
-      dataType: "json",
+      url: $('#select_team').find('option:selected').data('path'),
+      dataType: 'json',
     })
     // Ajaxリクエストが成功した場合
-    .done( (data) => {
+    .then(function (data) {
       $("#select_category").children().remove();
-      for(var i in data) {
-        $("#select_category").append("<option value=" +data[i].id + ">" + data[i].name +  "</option>");
-      }
+      data.forEach(function(element){
+        var option = $('<option>');
+        option.val(element.id);
+        option.text(element.name);
+        $("#select_category").append(option);
+      });
     })
     // Ajaxリクエストが失敗した場合
-    .fail( (data) => {
-      console.log('select_category ajax request failed');
+    .catch(function (data) {
+      alert('カテゴリの取得に失敗しました');
     });
   });
 });
